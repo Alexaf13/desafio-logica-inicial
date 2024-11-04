dbb854c6eebb17c8e2811c84cbada0ddb128104f
Alexaf13 <165784277+Alexaf13@users.noreply.github.com>
2024-11-04 15:47:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-let nomeDoHeroi = promt("AlexMan:");
-let xpHeroi = parseInt(prompt("5.000"));
+// Definindo os dados do herói
+let nomeDoHeroi = "AlexMan";  // Nome do herói
+let xpHeroi = 11000;           // Quantidade de XP do herói
 
 let nivel;
 
-if(xpHeroi <= 1.000){
-   nivel = "Ferro";
-} else if (xpHeroi >= 1.001 && xpHeroi <=2.000){
-   nivel = "Bronze";
-} else if (xpHeroi >= 2.001 && xpHeroi <=5.000){
+// Estrutura de decisão para classificar o nível do herói com base no XP
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
     nivel = "Prata";
-} else if (xpHeroi >= 5.001 && xpHeroi <=7.000){
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
     nivel = "Ouro";
-} else if (xpHeroi >= 7.001 && xpHeroi <=8.000){
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
     nivel = "Platina Diamante";
-} else if (xpHeroi >= 8.001 && xpHeroi <=9.000){
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
     nivel = "Ascendente";
-} else if (xpHeroi >= 9.001 && xpHeroi <=10.000){
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
     nivel = "Imortal";
 } else {
     nivel = "Radiante";
 }
 
-console.log(`O herói de nome ${nomeDoHeroi} está no nível de ${nivel}.`);
+// Exibe o resultado no console
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}.`);
